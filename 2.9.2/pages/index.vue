899fd2305d29f611bb59f3965ff01c8bdcaac9da
Coder911th <qqcoder911th@gmail.com>
2019-12-21 17:35:16 +0300

    init commit

---
+++ 2.9.2/pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="rect"></div>
+    <style>
+      .rect {
+        background-color: {{ color }};
+      }
+    </style>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color: 'red'
+    }
+  }
+}
+</script>
+
+<style>
+.rect {
+  width: 200px;
+  height: 100px;
+  margin: 10px auto;
+  border: 1px solid #000;
+}
+</style>
+
---
+++ README.md
@@ -0,0 +1,21 @@
+# Demo Nuxt.js SSR <style/> error
+
+## 2.9.2
+``` bash
+cd 2.9.2
+npm i
+npm run build
+npm start
+```
+Open http://localhost:3000
+The rectangle is red.
+
+## 2.11.0
+``` bash
+cd 2.11.0
+npm i
+npm run build
+npm start
+```
+Open http://localhost:3000
+The rectangle is white.
